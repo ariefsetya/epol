ccc922ab39707ef922716354883d7c118bd4728c
Arief Setya <ariefsetya@live.com>
2020-11-25 00:41:26 +0700

    update qr code

--- web/app/Http/Controllers/HomeController.php
+++ web/app/Http/Controllers/HomeController.php
@@ -281,8 +281,8 @@ class HomeController extends Controller
     public function downloadBarcode()
     {
         // Ghostscript::setGsPath("C:\Program Files (x86)\gs\gs8.64\bin\gswin32c.exe");
-        File::makeDirectory(public_path('/barcode/'.Session::get('event_id').'/'), $mode = 0777, true, true);
-        QrCode::format('png')->size(200)->generate($text, public_path('/barcode/'.Session::get('event_id').'/'.Auth::user()->reg_number.'.png'));
+        // File::makeDirectory(public_path('/barcode/'.Session::get('event_id').'/'), $mode = 0777, true, true);
+        // QrCode::format('png')->size(200)->generate($text, public_path('/barcode/'.Session::get('event_id').'/'.Auth::user()->reg_number.'.png'));
 
         File::makeDirectory(public_path('/pdf/'.Session::get('event_id').'/'), $mode = 0777, true, true);
         $pdf = PDF::loadView('print_pdf',['status'=>'print'])->setPaper([0,0,360,640], 'potrait');
@@ -297,9 +297,9 @@ class HomeController extends Controller
     }
     public function sendEmailBarcode()
     {
-        File::makeDirectory(public_path('/barcode/'.Session::get('event_id').'/'), $mode = 0777, true, true);
-        QrCode::format('png')->size(200)->generate($text, public_path('/barcode/'.Session::get('event_id').'/'.Auth::user()->reg_number.'.png'));
-        
+        // File::makeDirectory(public_path('/barcode/'.Session::get('event_id').'/'), $mode = 0777, true, true);
+        // QrCode::format('png')->size(200)->generate(, public_path('/barcode/'.Session::get('event_id').'/'.Auth::user()->reg_number.'.png'));
+
         File::makeDirectory(public_path('/pdf/'.Session::get('event_id').'/'), $mode = 0777, true, true);
         $pdf = PDF::loadView('print_pdf',['status'=>'print'])->setPaper([0,0,360,640], 'potrait');
         $pdf->save(public_path('/pdf/'.Session::get('event_id').'/'.Auth::user()->name.'.pdf'));
@@ -308,7 +308,8 @@ class HomeController extends Controller
     }
     public function qrcode($text)
     {
-        $response = Response::make(QrCode::format('png')->size(200)->generate($text), 200);
+        File::makeDirectory(public_path('/barcode/'.Session::get('event_id').'/'), $mode = 0777, true, true);
+        $response = Response::make(QrCode::format('png')->size(200)->generate($text, public_path('/barcode/'.Session::get('event_id').'/'.Auth::user()->reg_number.'.png'), 200);
         $response->header("Content-Type", 'image/png');
         return $response;
     }
