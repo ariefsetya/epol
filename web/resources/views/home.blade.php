d331a1ce27708d209549646577f0bb7a9ea76db5
Arief Setya <ariefsetya@live.com>
2020-12-02 13:57:23 +0700

    fix polling

--- web/resources/views/home.blade.php
+++ web/resources/views/home.blade.php
@@ -179,7 +179,7 @@
 
     @if(\App\EventDetail::where('event_id',Session::get('event_id'))->where('name','logout_button_visibility')->first()->content==1)
     @if(Auth::check())
-    @if(\App\EventDetail::where('event_id',Session::get('event_id'))->where('name','mode')->first()->content=='register_barcode' or (\App\EventDetail::where('event_id',Session::get('event_id'))->where('name','mode')->first()->content=='rsvp' and Auth::user()->rsvp->confirm_status == 1 and Auth::user()->email != '') or Auth::user()->rsvp->confirm_status==3)
+    @if(in_array(\App\EventDetail::where('event_id',Session::get('event_id'))->where('name','mode')->first()->content, ['polling_website','register_barcode']) or (\App\EventDetail::where('event_id',Session::get('event_id'))->where('name','mode')->first()->content=='rsvp' and (Auth::user()->rsvp->confirm_status ?? 0 == 1) and Auth::user()->email != '') or (Auth::user()->rsvp->confirm_status ?? 0 ==3))
     <br>
     <br>
     <a href="{{route('logout')}}" class="button" style="width: 100%">SELESAI</a>
