c4ccb2cfe4b489f8597b1859c87096d29a37506c
Arief Setya <ariefsetya@live.com>
2020-12-03 18:45:15 +0700

    check

---
+++ web/resources/views/polling_response/finish.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.guest')
+
+@section('content')
+
+<div class="col-md-3" style="margin:0 auto;">
+  <div style="margin-top:20%;">
+Selamat! Anda telah menyelesaikan Quiz
+<br>
+<br>
+Jawaban Benar : {{}}
