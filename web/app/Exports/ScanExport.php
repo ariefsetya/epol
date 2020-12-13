de4c857da8d0c3b1be8c9c659bd2b8467c6a089a
Arief Setya <ariefsetya@live.com>
2020-12-13 11:56:48 +0700

    fix import update

--- web/app/Exports/ScanExport.php
+++ web/app/Exports/ScanExport.php
@@ -16,8 +16,6 @@ class ScanExport implements FromCollection
     	$data = DB::select('select users.id, users.reg_number, users.email, users.phone, users.name,group_concat( concat(presences.via, " / ", presences.via_info," / ",presences.created_at)) as scan_info, count(users.id) from presences join users on users.id = presences.user_id
             where users.event_id='.Session::get('event_id').' and presences.via in ("scan","search") group by users.id');
 
-        dd($data);
-
         $arr[] = [
          'Kode',
          'Nama',
@@ -31,7 +29,7 @@ class ScanExport implements FromCollection
          $key->name,
          $key->email,
          $key->phone,
-         $key->scan_info
+         explode(",",$key->scan_info)[0]
      ];
  }
 
