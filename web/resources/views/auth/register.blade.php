3e5c22f747d68d4898bdb4c852c85f3d24cc0310
Arief Setya <ariefsetya@live.com>
2020-12-15 14:37:24 +0700

    fix validation

--- web/resources/views/auth/register.blade.php
+++ web/resources/views/auth/register.blade.php
@@ -9,18 +9,21 @@
     <input type="hidden" name="event_id" value="{{Session::get('event_id')}}">
 
     @if ($errors->any())
-    <div class="alert alert-danger">
-      <ul>
-        @foreach ($errors->all() as $error)
-        <li>{{ $error }}</li>
-        @endforeach
-      </ul>
+    <div class="info-box alert" style="width: 100%; height: auto; visibility: visible;">
+      <div class="info-box-content">
+        <ul style="list-style-type: none;">
+          @foreach ($errors->all() as $error)
+          <li>{{ $error }}</li>
+          @endforeach
+        </ul>
+      </div>
+      <span class="button square closer"></span>
     </div>
     @endif
     <input class="input-large mb-1" required type="text" name="name" id="name" data-role="input" placeholder="Nama Lengkap" value="{{ old('name') }}">
     <input class="input-large mb-1" required type="email" name="email" id="email" data-role="input" placeholder="Email" value="{{ old('email') }}">
     <input class="input-large mb-1" required type="number" name="phone" id="phone" data-role="input" data-prepend="+62" placeholder="Nomor WhatsApp" value="{{ old('phone') }}">
-    <button type="submit" class="button col-md-12 large">DAFTAR</button>
+    <button type="submit" class="button col-md-12 large primary">DAFTAR</button>
     <br>
   </form>
 </div>
