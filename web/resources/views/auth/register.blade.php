b9a843e536c9fad8fa51e74d1342621a7c5ff8b3
Arief Setya <ariefsetya@live.com>
2020-12-15 14:00:40 +0700

    update dbs

--- web/resources/views/auth/register.blade.php
+++ web/resources/views/auth/register.blade.php
@@ -8,10 +8,10 @@
     <input type="hidden" name="country_id" value="100">
     <input type="hidden" name="event_id" value="{{Session::get('event_id')}}">
 
-    <input class="input-large" required type="text" name="name" id="name" data-role="input" data-prepend="+62" placeholder="Nama Lengkap" value="{{ old('name') }}">
-    <input class="input-large" required type="email" name="email" id="email" data-role="input" data-prepend="+62" placeholder="Email" value="{{ old('email') }}">
-    <input class="input-large" required type="number" name="phone" id="phone" data-role="input" data-prepend="+62" placeholder="Nomor WhatsApp" value="{{ old('phone') }}">
-    <button type="submit" class="button col-md-12 large">SUBMIT</button>
+    <input class="input-large mb-1" required type="text" name="name" id="name" data-role="input" placeholder="Nama Lengkap" value="{{ old('name') }}">
+    <input class="input-large mb-1" required type="email" name="email" id="email" data-role="input" placeholder="Email" value="{{ old('email') }}">
+    <input class="input-large mb-1" required type="number" name="phone" id="phone" data-role="input" data-prepend="+62" placeholder="Nomor WhatsApp" value="{{ old('phone') }}">
+    <button type="submit" class="button col-md-12 large">DAFTAR</button>
     <br>
   </form>
 </div>
