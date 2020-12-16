1a2c9fb4248a0117b127cdc7b5da350a00f90c0b
Arief Setya <ariefsetya@live.com>
2020-12-16 16:21:56 +0700

    update dbs

--- web/resources/views/print_eticket.blade.php
+++ web/resources/views/print_eticket.blade.php
@@ -33,8 +33,21 @@
     </div>
     <div class="page-break"></div>
     <div @if(\App\EventDetail::where('event_id',Session::get('event_id'))->where('name','eticket_background_image_qr')->exists()) style="position: relative;background-image: url({{\App\EventDetail::where('event_id',Session::get('event_id'))->where('name','eticket_background_image_qr')->first()->content}});background-size: 100%;min-height: 100% !important;width:1000px;height:1497px;" @endif>
-
+        <div style="text-align: center;color: white;">
+            <img src="{{url('images/e4.png')}}">
+            <div>As a first time participant using DBS laptop, you must follow this One Time Webex Setup guide prior to the event. This will install Webex Meeting App from DBS Software Centre for optimal streaming experience!</div><br><br><div>On the day of the event, click on this hyperlink:</div><br>
+            <div><a href="https://dbs.webex.com/dbs/onstage/g.php?MTID=eadfc61710aea7d42d633412f4db7637d">CLICK HERE TO WATCH</a></div>
+            <br><br><div>Enter your First Name, Last Name, Email Address and click on [Join Now]</div><br><br>
+            <img src="{{url('images/e5.png')}}">
+            <br>
+            <div>Download Webex Meeting App</div><br>
+            <div>from Apple App Store or Google PlayStore</div><br>
+            <div>Click on Join Meeting & Input Meeting Number:</div><br>
+            <div style="color:yellow;">176 070 7355</div><br>
+            <div>Input Password: <span style="color:yellow;">App2020</span></div>
+        </div>
         <div style="position: absolute;bottom:0;width: 100%;font-family: Verdana;font-size: 20pt;padding: 10px;">
+
             <div style="display: inline-block;">
                 <img src="{{url('images/qrcode.jpeg')}}" style="width: 230px;">
             </div>
