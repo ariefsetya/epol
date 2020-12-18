65d5e803c5e4d4b9d7024e42ffcd684315cb14f6
Arief Setya <ariefsetya@live.com>
2020-12-18 14:37:11 +0700

    update

--- web/resources/views/quiz_response/display_report.blade.php
+++ web/resources/views/quiz_response/display_report.blade.php
@@ -1,49 +1,52 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title> </title>
-    <link rel="stylesheet" type="text/css" href="{{url('css/metro.css')}}">
-    <style type="text/css">
+  <title> </title>
+  <link rel="stylesheet" type="text/css" href="{{url('css/metro.css')}}">
+  <style type="text/css">
 
-        table tbody {
-          display: block;
-          max-height: 785px;
-          overflow-y: scroll;
-      }
-
-      table thead, table tbody tr {
-          display: table;
-          width: 100%;
-          table-layout: fixed;
-      }
-      *{
-        font-size: 35pt;
+    table tbody {
+      display: block;
+      max-height: 785px;
+      overflow-y: scroll;
     }
-</style>
+
+    table thead, table tbody tr {
+      display: table;
+      width: 100%;
+      table-layout: fixed;
+    }
+    *{
+      font-size: 30pt;
+    }
+    th{
+      color: white;
+    }
+  </style>
 </head>
 <body style="overflow:hidden;background-color: #2d89ef;">
-    <div style="width: 1920px;height:1080px;margin:auto;">
-        <div id="background" @if(\App\EventDetail::where('event_id',Session::get('event_id'))->where('name','quiz_report_background')->exists()) style="border:0px solid #000;width: 1920px;height:1080px;background-image:url({{\App\EventDetail::where('event_id',Session::get('event_id'))->where('name','quiz_report_background')->first()->content}});background-position: all;background-repeat: no-repeat; background-size: cover;" @endif>
-            <div style="height: 200px"></div>
-            <table class="table striped">
-                <thead>
-                    <th class="text-center" style="width:8%">No</th>
-                    <th class="text-center">E-Mail</th>
-                    <th class="text-center" style="width:10%">Skor</th>
-                    <th class="text-center" style="width:15%">Waktu</th>
-                </thead>
-                <tbody id="data">
-                  @foreach($report as $key => $row)
-                  <tr>
-                    <td class="text-center" style="width:8%">{{$key+=1}}</td>
-                    <td class="text-center">{{$row['email']}}</td>
-                    <td class="text-center" style="width:10%">{{$row['polling_response_count']}}</td>
-                    <td class="text-center" style="width:15%">{{date_format($row['created_at'],"H:i:s")}}</td>
-                  </tr>
-                  @endforeach
-                </tbody>
-            </table>
-        </div>
+  <div style="width: 1920px;height:1080px;margin:auto;">
+    <div id="background" @if(\App\EventDetail::where('event_id',Session::get('event_id'))->where('name','quiz_report_background')->exists()) style="border:0px solid #000;width: 1920px;height:1080px;background-image:url({{\App\EventDetail::where('event_id',Session::get('event_id'))->where('name','quiz_report_background')->first()->content}});background-position: all;background-repeat: no-repeat; background-size: cover;" @endif>
+      <div style="height: 200px"></div>
+      <table class="table striped">
+        <thead>
+          <th class="text-center" style="width:8%">No</th>
+          <th class="text-center">E-Mail</th>
+          <th class="text-center" style="width:10%">Skor</th>
+          <th class="text-center" style="width:15%">Waktu</th>
+        </thead>
+        <tbody id="data">
+          @foreach($report as $key => $row)
+          <tr>
+            <td class="text-center" style="width:8%">{{$key+=1}}</td>
+            <td class="text-center">{{$row['email']}}</td>
+            <td class="text-center" style="width:10%">{{$row['polling_response_count']}}</td>
+            <td class="text-center" style="width:15%">{{date_format($row['created_at'],"H:i:s")}}</td>
+          </tr>
+          @endforeach
+        </tbody>
+      </table>
     </div>
+  </div>
 </body>
 </html>
