c3b9993e578c3eb91af47f385f65c60f6f9ea8c8
Arief Setya <ariefsetya@live.com>
2020-12-18 14:30:11 +0700

    update

--- web/resources/views/polling/detail.blade.php
+++ web/resources/views/polling/detail.blade.php
@@ -3,8 +3,8 @@
 @section('content')
 <div class="container">
 	<br>
-    <h3 class="text-center" style="color:#000000;font-weight: bold;">{{$polling->id==5?"MOST REQUESTED SONGS":$polling_question->content}}</h3>
-    <hr>
+	<h3 class="text-center" style="color:#000000;font-weight: bold;">{{$polling->id==3?"IDOL TERFAVORIT":$polling_question->content}}</h3>
+	<hr>
 	<canvas id="myChart"></canvas>
 
 
@@ -23,108 +23,112 @@
 		},500);
 	});*/
 	var bgColor = [
-            'rgba(255, 99, 132, 0.7)',
-            'rgba(54, 162, 235, 0.7)',
-            'rgba(255, 206, 86, 0.7)',
-            'rgba(75, 192, 192, 0.7)',
-            'rgba(153, 102, 255, 0.7)',
-            'rgba(255, 159, 64, 0.7)'
-        ],
-        bdColor = [
-            'rgba(255, 99, 132, 1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)',
-            'rgba(75, 192, 192, 1)',
-            'rgba(153, 102, 255, 1)',
-            'rgba(255, 159, 64, 1)'
-        ];
+	'rgba(255, 99, 132, 0.7)',
+	'rgba(54, 162, 235, 0.7)',
+	'rgba(255, 206, 86, 0.7)',
+	'rgba(75, 192, 192, 0.7)',
+	'rgba(153, 102, 255, 0.7)',
+	'rgba(255, 159, 64, 0.7)'
+	],
+	bdColor = [
+	'rgba(255, 99, 132, 1)',
+	'rgba(54, 162, 235, 1)',
+	'rgba(255, 206, 86, 1)',
+	'rgba(75, 192, 192, 1)',
+	'rgba(153, 102, 255, 1)',
+	'rgba(255, 159, 64, 1)'
+	];
 	var ctx = document.getElementById('myChart');
 	
 	<?php $i=0; ?>
 
+	var data = {
+		labels:[
+		@foreach($polling_response as $row)
+		'{{$row->polling_answer->content}}',
+		@endforeach
+		],
+		datasets: [
+		{
+			data: [ 
+			@foreach($polling_response as $row)
+			'{{$row->total}}',
+			@endforeach
+			],
+			backgroundColor:[
+			@foreach($polling_response as $row)
+			bgColor[{{$i++}}],
+			@endforeach
+			],
+			<?php $i=0; ?>
+			backgroundColor:[
+			@foreach($polling_response as $row)
+			bdColor[{{$i++}}],
+			@endforeach
+			],
+			borderWidth: 1
+		},
+
+
+		]
+	};
+
 	var myChart = new Chart(ctx, {
-	    type: 'bar',
-	    data: {
-	    	labels:[''],
-	        datasets: [
-	        @foreach($polling_response as $row)
+		type: 'bar',
+		data: data,
+		options: {
+			"hover": {
+				"animationDuration": 0
+			},
+			"animation": {
+				"duration": 1,
+				"onComplete": function() {
+					var chartInstance = this.chart,
+					ctx = chartInstance.ctx;
 
-	        {
-	        	label:'{{$row->polling_answer->content}}',
-	            data: [ '{{$row->total}}'  ],
-	            backgroundColor:[bgColor[{{$i}}]],
-	            borderColor:[bdColor[{{$i++}}]],
-	            borderWidth: 1
-	        },
-	        
-	        @endforeach
+					ctx.font = Chart.helpers.fontString(25, Chart.defaults.global.defaultFontStyle, Chart.defaults.global.defaultFontFamily);
+					ctx.textAlign = 'center';
+					ctx.textBaseline = 'bottom';
 
-	        ]
-	    },
-	    options: {
-	        scales: {
-	            yAxes: [{
-	                ticks: {
-	                    beginAtZero: true,
-		                userCallback: function(label, index, labels) {
-		                    if (Math.floor(label) === label) {
-		                        return label;
-		                    }
+					this.data.datasets.forEach(function(dataset, i) {
+						var meta = chartInstance.controller.getDatasetMeta(i);
+						meta.data.forEach(function(bar, index) {
+							var data = dataset.data[index];
+							ctx.fillText(data, bar._model.x, bar._model.y - 5);
+						});
+					});
+				}
+			},
+			legend: {
+				"display": false
+			},
+			tooltips: {
+				"enabled": false
+			},
+			scales: {
+				yAxes: [{
+					display: false,
+					gridLines: {
+						display: false
+					},
+					ticks: {
+						max: Math.max(...data.datasets[0].data) + 10,
+						display: false,
+						beginAtZero: true
+					}
+				}],
+				xAxes: [{
+					gridLines: {
+						display: false
+					},
+					ticks: {
+						beginAtZero: true
+					}
+				}]
+			}
+		}
 
-		                }
-	                }
-	            }]
-	        },
-		    legend: {
-		        display: true,
-		        labels:{
-		        	fontSize:25,
-		        	
-			      	generateLabels: function(chart){
-			          var data = chart.data;
-			          var legends = Array.isArray(data.datasets) ? data.datasets.map(function(dataset, i) {
-			          	console.log(dataset);
-			            return {
-			              text: dataset.label+" ("+dataset.data+")",
-			              fillStyle: (!Array.isArray(dataset.backgroundColor) ? dataset.backgroundColor : dataset.backgroundColor[0]),
-			              hidden: !chart.isDatasetVisible(i),
-			              lineCap: dataset.borderCapStyle,
-			              lineDash: dataset.borderDash,
-			              lineDashOffset: dataset.borderDashOffset,
-			              lineJoin: dataset.borderJoinStyle,
-			              lineWidth: dataset.borderWidth,
-			              strokeStyle: dataset.borderColor,
-			              pointStyle: dataset.pointStyle,
-
-			              // Below is extra data used for toggling the datasets
-			              datasetIndex: i
-			            };
-			          }, this) : [];
-			          return legends;
-			        }
-		        },
-		        align:'center',
-		        position:'right'
-		    },
-
-		    showTooltips: false,
-		    onAnimationComplete: function () {
-
-		        var ctx = this.chart.ctx;
-		        ctx.font = this.scale.font;
-		        ctx.fillStyle = this.scale.textColor
-		        ctx.textAlign = "center";
-		        ctx.textBaseline = "bottom";
-
-		        this.datasets.forEach(function (dataset) {
-		            dataset.bars.forEach(function (bar) {
-		                ctx.fillText(bar.value, bar.x, bar.y - 5);
-		            });
-		        })
-		    }
-	    }
-		    
 	});
 
-	</script>
+</script>
 @endsection
